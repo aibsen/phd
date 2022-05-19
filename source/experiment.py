d930ee2332fada214a7e4e1763e59cc24540f6ed
Amanda Ibsen <amanda.ibsen@gmail.com>
2022-05-19 14:39:45 -0400

    removed .cpu() operations for efficiency

--- source/experiment.py
+++ source/experiment.py
@@ -12,6 +12,8 @@ import pandas as pd
 import time
 from torch.utils.data import SequentialSampler
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+from torchmetrics import Accuracy, F1Score
+from torchmetrics.functional import precision_recall
 # from utils import save_to_stats_pkl_file, load_from_stats_pkl_file, \
 #     save_statistics, load_statistics, save_classification_results
 
@@ -19,7 +21,8 @@ from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 class Experiment(nn.Module):
     def __init__(self, network_model, 
         experiment_name,metric="f1_score", 
-        num_epochs=100, 
+        num_epochs=100,
+        num_output_classes=14, 
         learning_rate=1e-03,
         batch_size = 64, 
         train_data=None, 
@@ -29,35 +32,31 @@ class Experiment(nn.Module):
         val_sampler=None,
         test_sampler = None,
         weight_decay_coefficient=1e-03, 
-        use_gpu=True, 
         continue_from_epoch=-1, 
         best_idx=0, 
-        verbose=True,
         cached_dataset=False,
-        patience=5,
+        patience=3,
         validation_step=5,
         class_weights = None):
 
         super(Experiment, self).__init__()
 
-        if torch.cuda.is_available() and use_gpu:
+        if torch.cuda.is_available():
             self.device = torch.device('cuda')
             os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-            if verbose:
-                print("using GPU")
-        else:
-            if verbose:
-                print("using CPU")
-            self.device = torch.device('cpu')
+            # print("using GPU")
 
         self.metric = metric
-        self.verbose = verbose
         self.experiment_name = experiment_name
         self.model = network_model
         self.model.to(self.device)
         self.model.reset_parameters()
         self.patience = patience
         self.validation_step = validation_step
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.break_epoch = num_epochs
+        self.num_output_classes = num_output_classes
 
         self.train_data=None
         self.val_data=None
@@ -87,6 +86,9 @@ class Experiment(nn.Module):
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate, amsgrad=False,
                                     weight_decay=weight_decay_coefficient)
+        if class_weights:
+            class_weights = torch.FloatTensor(class_weights).cuda()
+        self.criterion = nn.CrossEntropyLoss(weight=class_weights).to(self.device)  # send the loss computation to the GPU
 
         # Generate the directory names
         self.experiment_folder = os.path.abspath(experiment_name)
@@ -102,12 +104,6 @@ class Experiment(nn.Module):
             os.mkdir(self.experiment_logs)  # create the experiment log directory
             os.mkdir(self.experiment_saved_models)  # create the experiment saved models directory
 
-        self.num_epochs = num_epochs
-        self.break_epoch = num_epochs
-        if class_weights:
-            class_weights = torch.FloatTensor(class_weights).cuda()
-        self.criterion = nn.CrossEntropyLoss(weight=class_weights).to(self.device)  # send the loss computation to the GPU
-
         if continue_from_epoch != -1:  # if continue from epoch is not -1 then
             try:
                 self.best_val_model_idx, self.best_val_model_f1 = self.load_model(
@@ -116,6 +112,9 @@ class Experiment(nn.Module):
                 self.starting_epoch = continue_from_epoch
             except:
                 print("Did not save that epoch's model, will start from 0")
+
+        self.accuracy = Accuracy(num_classes = self.num_output_classes, average = 'micro')
+        self.f1 = F1Score(num_classes = self.num_output_classes, average='macro')
 
     def run_train_iter(self, x, y):
         self.train()
@@ -126,10 +125,7 @@ class Experiment(nn.Module):
         self.optimizer.step()
         predicted = F.log_softmax(out.data,dim=1)
         predicted = torch.argmax(predicted, 1)
-        loss = loss.data.cpu().numpy()
-        y_cpu = list(y.data.cpu().numpy())
-        predicted_cpu = list(predicted.cpu().numpy())
-        return loss, predicted_cpu, y_cpu
+        return loss, predicted
 
     def run_evaluation_iter(self, x, y):
         self.eval()  # sets the system to validation mode
@@ -137,10 +133,7 @@ class Experiment(nn.Module):
         loss =  self.criterion(out,y)
         predicted = F.log_softmax(out.data,dim=1)
         predicted = torch.argmax(predicted, 1)
-        loss = loss.data.cpu().numpy()
-        y_cpu = list(y.data.cpu().numpy())
-        predicted_cpu = list(predicted.cpu().numpy())
-        return loss, predicted_cpu, y_cpu
+        return loss, predicted
 
     def save_model(self, model_save_dir, model_save_name, model_idx, best_validation_model_idx):
         state = dict()
@@ -237,95 +230,119 @@ class Experiment(nn.Module):
         
     def run_train_phase(self):
         start_time = time.time()
-        train_stats = {"epoch": [],"train_acc": [], "train_loss": [], "train_f1":[],"train_precision":[],"train_recall":[]}  # initialize a dict to keep the per-epoch metrics
-        val_stats = {"epoch": [],"val_acc": [],"val_loss": [], "val_f1":[], "val_precision":[],"val_recall":[]}
-        
-        strike = -1
+
+        strike = 0
         step_count = 0
-        val_loss = np.Inf #initial value
         last_val_performance = 0
+        last_train_cm = torch.zeros((len(self.train_data.dataset),3), dtype=torch.int) # holds ids, preds, targets
+        current_val_cm = torch.zeros((len(self.val_data.dataset),3), dtype=torch.int) # holds ids, preds, targets
+        best_val_cm = torch.zeros((len(self.val_data.dataset),3), dtype=torch.int) # holds ids, preds, targets
+        val_stats = torch.full((int(self.num_epochs/self.validation_step),6),-1, dtype=torch.float) # holds epoch, acc, loss, f1, precission, recall, per validation epoch
+        train_stats = torch.full((int(self.num_epochs),6),-1, dtype=torch.float) # holds epoch, acc, loss, f1, precission, recall, per train epoch
+        val_idx = 0
+        train_n_batches = len(self.train_data)
+        val_n_batches = len(self.val_data)
+        val_loss = -1
+
 
         with tqdm.tqdm(total=self.num_epochs) as pbar_train:
 
             for i, epoch_idx in enumerate(range(self.starting_epoch, self.num_epochs)):
                 
                 step_count+=1
-                train_loss_cum = 0 
-                train_predictions = []
-                train_targets = []
-                n_batches = len(self.train_data)
+                running_train_loss = 0.0
 
                 for idx, (x, y,ids) in enumerate(self.train_data):
-                    loss, pred, targets = self.run_train_iter(x=x, y=y)
-                    train_loss_cum += loss
-                    train_predictions += pred
-                    train_targets += targets                
+                    loss, pred = self.run_train_iter(x=x, y=y)
+                    last_train_cm[idx*self.batch_size:(idx+1)*self.batch_size,0] = ids
+                    last_train_cm[idx*self.batch_size:(idx+1)*self.batch_size,1] = pred
+                    last_train_cm[idx*self.batch_size:(idx+1)*self.batch_size,2] = y
+                    running_train_loss += loss.item()
                 
-                train_loss = train_loss_cum/n_batches
-                accuracy = accuracy_score(train_targets, train_predictions)
-                precision, recall, f1, _ = precision_recall_fscore_support(train_targets, train_predictions, average = 'macro', zero_division=0)
-                
-                train_stats['train_acc'].append(accuracy)
-                train_stats['train_loss'].append(train_loss)
-                train_stats['train_f1'].append(f1)
-                train_stats['train_recall'].append(recall)
-                train_stats['train_precision'].append(precision)
-                train_stats['epoch'].append(epoch_idx)
+                preds, targets = last_train_cm[:,1],last_train_cm[:,2]
+                precision, recall = precision_recall(preds,targets, num_classes=self.num_output_classes, average='macro')
+                f1 = self.f1(preds,targets)
+                train_loss = running_train_loss/train_n_batches
+                train_stats[epoch_idx,0] = epoch_idx #epoch
+                train_stats[epoch_idx,1] = self.accuracy(preds,targets) #accuracy
+                train_stats[epoch_idx,2] = train_loss #loss
+                train_stats[epoch_idx,3] = f1 #f1
+                train_stats[epoch_idx,4] = precision # precision
+                train_stats[epoch_idx,5] = recall #recall
             
                 if step_count == self.validation_step:
-                    step_count = 0
-                    # with tqdm.tqdm(total=len(self.val_data)) as pbar_val:
 
-                    val_loss_cum = 0 
-                    val_predictions = []
-                    val_targets = []
-                    n_batches = len(self.val_data)
+                    step_count = 0
+                    running_val_loss = 0.0
 
                     for idx, (x, y, ids) in enumerate(self.val_data):
-                        loss, pred, targets = self.run_evaluation_iter(x=x, y=y)
-                        val_loss_cum += loss
-                        val_predictions += pred
-                        val_targets += targets
-                        
-                    val_loss = val_loss_cum/n_batches
-                    accuracy = accuracy_score(val_targets, val_predictions)
-                    precision, recall, f1, _ = precision_recall_fscore_support(val_targets, val_predictions, average = 'macro', zero_division=0)
-                    
-                    val_stats['val_acc'].append(accuracy)
-                    val_stats['val_loss'].append(val_loss)
-                    val_stats['val_f1'].append(f1)
-                    val_stats['val_recall'].append(recall)
-                    val_stats['val_precision'].append(precision)
-                    val_stats['epoch'].append(epoch_idx)
+                        loss, pred = self.run_evaluation_iter(x=x, y=y)
+                        current_val_cm[idx*self.batch_size:(idx+1)*self.batch_size,0] = ids
+                        current_val_cm[idx*self.batch_size:(idx+1)*self.batch_size,1] = pred
+                        current_val_cm[idx*self.batch_size:(idx+1)*self.batch_size,2] = y
+                        running_val_loss += loss.item()
 
+                    preds, targets = current_val_cm[:,1],current_val_cm[:,2]
+                    precision, recall = precision_recall(preds,targets, num_classes=self.num_output_classes, average='macro')
+                    f1 = self.f1(preds,targets)
+                    val_loss = running_val_loss/val_n_batches
+                    val_stats[val_idx,0] = epoch_idx #epoch
+                    val_stats[val_idx,1] = self.accuracy(preds,targets) #accuracy
+                    val_stats[val_idx,2] = val_loss #loss
+                    val_stats[val_idx,3] = f1 #f1
+                    val_stats[val_idx,4] = precision # precision
+                    val_stats[val_idx,5] = recall #recall
+                    val_idx+=1
+                    # print(val_stats)
+                    # print(f1)
                 #if the f1-score of the current epoch for the validation set is better than previous epochs
                     if f1 > self.best_val_model_f1:
+                        best_val_cm = current_val_cm
                         self.best_val_model_f1 = f1
                         self.best_val_model_idx = epoch_idx
                         self.save_model(model_save_dir=self.experiment_saved_models,
                             model_save_name="epoch", model_idx=epoch_idx,best_validation_model_idx=self.best_val_model_idx)
                         self.break_epoch = epoch_idx
-                        strike = -1
+                        strike = 0
+                        # print(f1.item())
 
                     elif f1 < last_val_performance:
                         strike+=1
+                        # print(f1.item())
                         # print("strike {} at ep {}".format(strike, i))
-
                     last_val_performance = f1
 
                 pbar_train.update(1)
                 elapsed_time = time.time() - start_time 
-                pbar_train.set_description("Tr/Val loss: {:.3f}/{:.3f}, ET {:.2f}s".format(train_loss,val_loss,elapsed_time))
+                pbar_train.set_description("Tr/Val loss: {:.3f}/{:.3f}, Strike: {}, ET {:.2f}s".format(train_loss,val_loss,strike,elapsed_time))
+                # pbar_train.set_description("Strikes: {}, ET {:.2f}s".format(strike,elapsed_time))
 
                 if strike == self.patience:
                     # print("Early stop, model is overfitting")
                     break
 
         #save statistics at the end only
-        self.save_statistics(experiment_log_dir=self.experiment_logs, filename='training_summary.csv',
-                stats_dict=train_stats)  # save statistics    
-        self.save_statistics(experiment_log_dir=self.experiment_logs, filename='validation_summary.csv',
-                stats_dict=val_stats)  # save statistics    
+        self.save_train_val_statistics0(train_stats, val_stats, last_train_cm, best_val_cm)
+    
+    def save_train_val_statistics0(self, train_stats, val_stats, train_last_cm, val_last_cm):
+        
+        stats_keys = ['epoch', 'accuracy', 'loss', 'f1', 'precision', 'recall']
+        train_stats = train_stats.cpu().numpy()
+        val_stats = val_stats.cpu().numpy()
+        train_df = pd.DataFrame(train_stats, columns = stats_keys)
+        train_df.epoch = train_df.epoch.astype(int)
+        val_df = pd.DataFrame(val_stats, columns = stats_keys)
+        val_df.epoch = val_df.epoch.astype(int)
+        train_df.to_csv(self.experiment_logs+'/training_summary.csv',sep=',',index=False)
+        val_df.to_csv(self.experiment_logs+'/validation_summary.csv',sep=',',index=False)
+
+        results_keys = ['object_id','prediction','target']
+        train_cm = train_last_cm.cpu().numpy()
+        val_cm = val_last_cm.cpu().numpy()
+        train_cm_df = pd.DataFrame(train_cm, columns=results_keys)
+        val_cm_df = pd.DataFrame(val_cm, columns=results_keys)
+        train_cm_df.to_csv(self.experiment_logs+'/train_results.csv',sep=',',index=False)
+        val_cm_df.to_csv(self.experiment_logs+'/validation_results.csv',sep=',',index=False)
 
     def run_experiment(self):
         """
@@ -343,8 +360,7 @@ class Experiment(nn.Module):
             self.run_final_train_phase([self.train_data,self.val_data],self.experiment_logs)
 
         if self.test_data:
-            if self.verbose:
-                print("")
-                print("Starting test phase")
-                print("")
+            print("")
+            print("Starting test phase")
+            print("")
             self.run_test_phase(self.test_data, self.break_epoch,self.experiment_logs)
