0ba574184235ca0c1286db38ce6579d2d9b0d0b8
Amanda Ibsen <amanda.ibsen@gmail.com>
2022-06-30 14:32:56 -0400

    this script gets modified as I go along

--- source/scripts/run_plasticc_dmdt_test.py
+++ source/scripts/run_plasticc_dmdt_test.py
@@ -11,13 +11,14 @@ from data_samplers import CachedRandomSampler
 from experiment import Experiment
 # from plot_utils import *
 from torchvision import transforms
-from transforms import RandomCrop,ZeroPad,RightCrop,RandomCropsZeroPad
+from transforms import GroupClass
 from recurrent_models import GRU1D
 from convolutional_models import CNN
 from dmdt_cnns import DMDTShallowCNN, DMDTCNN, VanillaCNN
 from seeded_experiment import SeededExperiment
 from experiment import Experiment 
 import matplotlib.pyplot as plt
+import gc
 
 from torch.utils.data import RandomSampler
 
@@ -33,7 +34,7 @@ data_dir_test = "/home/ai/phd/data/plasticc/dmdt/test/"
 
 # exp_name = results_dir+"dummy"
 
-batch_size = 64
+batch_size = 32
 num_epochs = 60
 use_gpu = True
 lr = 1e-04
@@ -60,16 +61,38 @@ drop_out_linear = 0.25
 out_linear = 128
 
 exp_name = results_dir+"{}_{}x{}final".format(arch_code,lc_length,lc_length)
-training_data_file=data_dir_training+'dmdts_training_{}x{}_b{}augmented_noise{}.h5'.format(lc_length,lc_length,b_code,noise)
-print(training_data_file)
+# training_data_file=data_dir_training+'dmdts_training_{}x{}_b{}augmented_noise{}.h5'.format(lc_length,lc_length,b_code,noise)
+# print(training_data_file)
 # train_dataset = LCs(lc_length,training_data_file,n_channels=6)
 # train_dataset.load_data_into_memory()
 input_shape = torch.Size([6, 24, 24])
 
+
+def run_test_batch(experiment,i):
+    test_data_file=data_dir_test+'dmdts_test_{}x{}_b{}{}_no99.h5'.format(lc_length,lc_length,b_code,i)
+    # test_data_file=data_dir_test+'dmdts_test_{}x{}_b{}_{}.h5'.format(lc_length,lc_length,b_code,i)
+    print(test_data_file)
+    test_dataset = LCs(lc_length,test_data_file,n_channels=6)
+    # test_dataset = LCs(lc_length,test_data_file,n_channels=6, transform=transform)
+    print("loading dataset")
+    test_dataset.load_data_into_memory()
+    print(torch.cuda.mem_get_info(device=None))
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    experiment.test_data = test_loader
+    experiment.run_test_phase(test_loader, load_model=False, data_name="test_{}_no99_tame".format(i))
+    print(torch.cuda.mem_get_info(device=None))
+    print("releasing cache")
+    del test_dataset
+    del test_loader
+    del experiment.test_data
+    torch.cuda.empty_cache()
+    # gc.collect()
+    print(torch.cuda.mem_get_info(device=None))    
+
 ####DMDT Plasticc
 cnn_params={
 'input_shape': input_shape,
-'num_output_classes': 14,
+'num_output_classes': 15,
 'n_conv_layers': n_conv_layers,
 'kernel_size': kernel_size,
 'n_filters': n_filters,
@@ -88,7 +111,7 @@ exp_params={
     "use_gpu" : use_gpu,
     "batch_size" : batch_size,
     "chunked": False,
-    "num_output_classes": 14,
+    "num_output_classes": 15,
     "network_model":network,
     "patience":5,
     "validation_step":3
@@ -105,18 +128,15 @@ experiment = Experiment(
     num_output_classes=exp_params["num_output_classes"],
 )
 
-# train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-# experiment.run_final_train_phase([train_loader])
-experiment.load_model(exp_name+"/saved_models","final_model.pth.tar")
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+experiment.run_final_train_phase([train_loader],data_name='final_training_no_99', model_name="final_model_no_99.pth.tar")
+print(torch.cuda.mem_get_info(device=None))
+print("loading model")
+experiment.load_model(exp_name+"/saved_models","final_model_no_99.pth.tar")
+print(torch.cuda.mem_get_info(device=None))
+# transform = GroupClass(14,14)
 
-for i in range(7,8):
-    test_data_file=data_dir_test+'dmdts_test_{}x{}_b{}{}_no99.h5'.format(lc_length,lc_length,b_code,i)
-    print(test_data_file)
-    test_dataset = LCs(lc_length,test_data_file,n_channels=6)
-    test_dataset.load_data_into_memory()
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    experiment.test_data = test_loader
-    experiment.run_test_phase(test_loader, load_model=False, data_name="test_{}".format(i))
-
+for i in range(1,12):
+    run_test_batch(experiment,i)
 
 
