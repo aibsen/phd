bd5793d2665339a38d74ca7daff93e8a6ed7fb10
Amanda Ibsen <amanda.ibsen@gmail.com>
2022-12-02 03:46:37 -0300

    green

--- source/plot_utils.py
+++ source/plot_utils.py
@@ -288,7 +288,7 @@ def draw_cm(ax,target, prediction, normalized=True, colormap=None, names=plastic
     if colormap is not None:
         im = ax.imshow(cm, interpolation= 'nearest', cmap=colormap)
     else : 
-        im = ax.imshow(cm, interpolation= 'nearest', cmap=plt.cm.Oranges)
+        im = ax.imshow(cm, interpolation= 'nearest', cmap=plt.cm.Greens)
     fmt ='.2f'
     thresh = cm.max() / 2.
     for i in range(cm.shape[0]):
